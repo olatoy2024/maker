33e17523548526ce8d05a13ea5a1fc98ed5076cf
Toyyib <olalekanoladejo2000@gmail.com>
2025-05-15 13:27:31 +0100

    fixing javascript sign up function

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let eyeIcon = document.getElementById("eyeicon");
 const password = document.getElementById("password");
 const confirmPassword = document.querySelector("#confirmPassword");
 const eyeIconTwo = document.getElementById("eyeicon2");
+
 eyeIcon.onclick = function () {
   if ((password.type === "password") === "password") {
     password.type = "text";
@@ -25,3 +26,30 @@ eyeIconTwo.onclick = function () {
     eyeIcon.src = "./images/password-close-blind.png";
   }
 };
+
+form.addEventListener("submit", (e) => {
+  // e.preventDefault();
+
+  const errors = [];
+
+  if (fullName) {
+    // if we have the full Name input then, we are in the sign up
+
+    errors = getSignUpFormErrors(
+      fullName.value,
+      email.value,
+      password.value,
+      confirmPassword.value
+    );
+  } else {
+    // if we don't have full name input, then we are in the sign in button
+    errors = getSignInFormErrors(email.value, password.value);
+  }
+});
+
+function getSignUpFormErrors(fullName, email, password, confirmPassword) {
+  let errors = [];
+  if (!fullName === "" || fullName === null) {
+    errors.push("Full Name is Required");
+  }
+}
