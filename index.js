4fbe8b970feebdfe894d8392a1936f4000a45ba0
Toyyib <olalekanoladejo2000@gmail.com>
2025-05-15 12:25:29 +0100

    fix the hidden eye icon on the confirmPassword

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+const form = document.getElementById("form");
+const fullName = document.querySelector("#fullName");
+const email = document.getElementById("email");
 let eyeIcon = document.getElementById("eyeicon");
-let password = document.getElementById("password");
-
+const password = document.getElementById("password");
+const confirmPassword = document.querySelector("#confirmPassword");
+const eyeIconTwo = document.getElementById("eyeicon2");
 eyeIcon.onclick = function () {
-  if (password.type === "password") {
+  if ((password.type === "password") === "password") {
     password.type = "text";
+
     eyeIcon.src = "/images/eye-line.svg";
   } else {
     password.type = "password";
     eyeIcon.src = "/images/password-close-blind.png";
+  }
+};
+
+eyeIconTwo.onclick = function () {
+  if (confirmPassword.type === "password") {
+    confirmPassword.type = "text";
+    eyeIconTwo.src = "./images/eye-line.svg";
+  } else {
+    confirmPassword.type = "password";
+    eyeIcon.src = "./images/password-close-blind.png";
   }
 };
